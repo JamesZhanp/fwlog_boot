79717dbb2654f63e020b918fc7d78a0803f4a743
JamesZhanp <15906654081@163.com>
2018-01-24 13:30:04 +0800

    new test

--- src/main/java/com/fwlog/james/analysis/statisticsAnalysis/StatisticsAnalysis.java
+++ src/main/java/com/fwlog/james/analysis/statisticsAnalysis/StatisticsAnalysis.java
@@ -4,9 +4,9 @@ import com.fwlog.james.analysis.preAnalysis.Pretreatment;
 import com.fwlog.james.entity.Fwlog;
 import com.fwlog.james.service.FwlogService;
 import com.fwlog.james.utils.SpringUtil;
-import org.junit.Test;
 import org.springframework.context.annotation.Configuration;
 
+import javax.naming.ldap.SortResponseControl;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.LinkedList;
@@ -17,7 +17,7 @@ import java.util.List;
  * 一个完成的访问事件中的源IP和目的IP的访问的平均值和方差，与阈值相比较，得出异常访问的记录
  * 文件之间直接传参，尽量减少IO请求
  * @author jamesZhan
- * @date 2018-01-23
+ * @Date 2018-01-23
  */
 @Configuration
 public class StatisticsAnalysis {
@@ -29,9 +29,11 @@ public class StatisticsAnalysis {
     private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
     //        保存每秒各个IP地址的访问次数
     private static List<IP> srcIpList = new LinkedList<>();
+//    在动态数组当中查找是否存在该元素
+    private static boolean flag = false;
     private static List<IP> destIpList = new LinkedList<>();
 //    private  List<Fwlog> fwlogList = Pretreatment.fwlogList;
-    public static void statisticsAnalysis(Fwlog fwlog) throws Exception{
+    private static void statisticsAnalysis(Fwlog fwlog) throws Exception{
 
 //        for debug
 //        System.out.println(fwlogList.size());
@@ -46,6 +48,7 @@ public class StatisticsAnalysis {
         Long[] ip = new Long[2];
         ip[0] = fwlog.getSrcip();
         ip[1] = fwlog.getDestip();
+//        当动态数组为空时，插入首条数据
         if(srcIpList.size() == 0){
             IP ip1 = new IP();
             ip1.setsTime(lastTime);
@@ -64,57 +67,67 @@ public class StatisticsAnalysis {
             destIpList.add(ip1);
         }
 //        统计每秒的原始IP的访问情况
-        for (int i = 0 ; i < srcIpList.size() ; i++){
+        for (IP ip1 : srcIpList){
             //寻找是否有IP地址一样，时间一样的访问
-            if ((srcIpList.get(i).getValue() == ip[0]) &&
-                    (srcIpList.get(i).getsTime().getTime() == lastTime.getTime())){
-                Integer time = srcIpList.get(i).getCount();
+            if ((ip1.getValue().equals(ip[0])) &&
+                    (ip1.getsTime().getTime() == lastTime.getTime())){
+                flag = true;
+                Integer time = ip1.getCount();
                 time += 1;
-                srcIpList.get(i).setCount(time);
-            }else{
-                IP ip1 = new IP();
-                ip1.setCount(fwlog.getNumber());
-                ip1.setValue(ip[0]);
-                ip1.setsTime(lastTime);
-                ip1.seteTime(new Date(lastTime.getTime() + MIN_INTERVAL));
-                srcIpList.add(ip1);
+                ip1.setCount(time);
             }
         }
-
+//        动态数组当中不存在该IP情况
+        if (!flag){
+            IP ip2 = new IP();
+            ip2.setCount(fwlog.getNumber());
+            ip2.setValue(ip[0]);
+            ip2.setsTime(lastTime);
+            ip2.seteTime(new Date(lastTime.getTime() + MIN_INTERVAL));
+            srcIpList.add(ip2);
+        }
 //        统计每秒的目的IP的访问情况
-        for (int i = 0 ; i < destIpList.size() ; i++){
-            if ((destIpList.get(i).getValue() == ip[1]) &&
-                    (destIpList.get(i).getsTime().getTime() == lastTime.getTime())){
-                Integer time = srcIpList.get(i).getCount();
+        for (IP ip1 : destIpList){
+            if ((ip1.getValue().equals(ip[1])) &&
+                    ip1.getsTime().getTime() == lastTime.getTime()){
+                Integer time = ip1.getCount();
                 time += 1;
-                destIpList.get(i).setCount(time);
-            }else{
-                IP ip1 = new IP();
-                ip1.setCount(fwlog.getNumber());
-                ip1.setValue(ip[1]);
-                ip1.setsTime(lastTime);
-                ip1.seteTime(new Date(lastTime.getTime() + MIN_INTERVAL));
-                destIpList.add(ip1);
+                ip1.setCount(time);
+                flag = true;
             }
         }
-
+        if (!flag){
+            IP ip2 = new IP();
+            ip2.setCount(fwlog.getNumber());
+            ip2.setValue(ip[1]);
+            ip2.setsTime(lastTime);
+            ip2.seteTime(new Date(lastTime.getTime() + MIN_INTERVAL));
+            destIpList.add(ip2);
+        }
         //保存至数据库
-
     }
 
     public void analysis(){
         FwlogService fwlogService = (FwlogService) SpringUtil.getBean("fwlogService");
 
         List<Fwlog> fwlogs = fwlogService.getAll();
-        for (int i = 0 ; i < fwlogs.size() ; i++){
+        for (Fwlog fwlog : fwlogs){
             try{
-                 StatisticsAnalysis.statisticsAnalysis(fwlogs.get(i));
+                 StatisticsAnalysis.statisticsAnalysis(fwlog);
+//                 每次循环结束之后，将flag置为false
+                 flag = false;
             }catch (Exception e){
                 e.printStackTrace();
             }
         }
+        //当动态数组为空时插入数据，此处进行回原
+        srcIpList.get(0).setCount(srcIpList.get(0).getCount() - 1);
+        destIpList.get(0).setCount(destIpList.get(0).getCount() - 1);
+
+//        for debug
+//        System.out.println(srcIpList.size());
+//        System.out.println(destIpList.size());
         //在计算出这个访问事件下的各个IP的访问的均值，方差等
-        System.out.println(srcIpList.size());
-        System.out.println(destIpList.size());
+
     }
 }
